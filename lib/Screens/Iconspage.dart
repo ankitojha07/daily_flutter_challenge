77cb8e8297d285f1f1b98d024baebe9bcd803bce
Ankit Ojha <ankitojha1409@gmail.com>
2023-02-19 12:32:41 +0530

    font awesome icons added

--- lib/Screens/Iconspage.dart
+++ lib/Screens/Iconspage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Iconpage extends StatelessWidget {
   const Iconpage({Key? key}) : super(key: key);
@@ -8,16 +9,29 @@ class Iconpage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.lightGreen,
       body: Center(
-          child: IconButton(
-            icon: Icon(
-                  Icons.alt_route_rounded,
-                ),
-                onPressed: (){},
+        // this is font awesome icons --------------------------
+        child : IconButton(
+            icon: Icon(FontAwesomeIcons.arrowPointer),
+            onPressed: (){},
                 iconSize: 100,
                 color: Colors.lightGreenAccent,
                 splashColor: Colors.white,
                 highlightColor: Colors.red,
-          )),
+          )
+      )
+
+// this is material icon -------------------------
+          // child: IconButton(
+          //   icon: Icon(
+          //         Icons.alt_route_rounded,
+          //       ),
+          //       onPressed: (){},
+          //       iconSize: 100,
+          //       color: Colors.lightGreenAccent,
+          //       splashColor: Colors.white,
+          //       highlightColor: Colors.red,
+          // )),
+          
     );
   }
 }
