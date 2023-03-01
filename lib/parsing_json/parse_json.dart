b44c7382188d8500fd966187ed1a943487d6219c
Ankit Ojha <ankitojha1409@gmail.com>
2023-03-02 01:03:22 +0530

    JSON Parsing

---
+++ lib/parsing_json/parse_json.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ParseJson extends StatefulWidget {
+  const ParseJson({Key? key}) : super(key: key);
+
+  @override
+  State<ParseJson> createState() => _ParseJsonState();
+}
+
+class _ParseJsonState extends State<ParseJson> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
